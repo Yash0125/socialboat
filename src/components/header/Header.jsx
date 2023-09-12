29abfa99541e96947b47b7e2a4320bce0feb7e33
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-09-12 22:51:30 +0530

    feat : header created

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./header.css";
+
+const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const fetchVideos = async () => {
+    try {
+      const response = await fetch(
+        `https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${searchQuery}&numResults=10`
+      );
+      const data = await response.json();
+      setResults(data.results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching videos:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (searchQuery.trim() !== "") {
+      fetchVideos();
+    } else {
+      setResults([]);
+    }
+  }, [searchQuery]);
+
+  return (
+    <>
+      <header className="header">
+        <div className="logo"> SocialBoat </div>
+        <input
+          type="text"
+          placeholder="Search for videos..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <img
+          src="https://ik.imagekit.io/socialboat/tr:w-800,c-maintain_ratio,fo-auto/Screenshot_2022-11-18_at_6.58_1_9uEHwjAKg.png?ik-sdk-version=javascript-1.4.3&updatedAt=1668867345853"
+          alt="Profile"
+          className="profile-image"
+        />
+      </header>
+      <h1 className="search-text">
+        Search for : <span className="search">{searchQuery}</span>
+      </h1>
+    </>
+  );
+};
+
+export default Header;
